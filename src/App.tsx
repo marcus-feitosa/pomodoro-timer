da60db34a899a39e47a8586d995b8b2374c58a98
marcus_feitosa <marcusfeitosa02@gmail.com>
2023-01-26 15:29:16 -0300

    rotas da aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,17 @@
-import { GlobalStyle } from "./styles/global"
-import { DefaultTheme } from "styled-components"
+import { GlobalStyle } from './styles/global'
+import {ThemeProvider } from 'styled-components'
+import { BrowserRouter } from 'react-router-dom'
+import { defaultTheme } from './styles/theme/default'
+import { Router } from './Router'
 
 export function App() {
   return(
-    <h1>Pomodoro Timer</h1>
+  <ThemeProvider theme={defaultTheme}>
+    <BrowserRouter>
+      <Router/>
+      <GlobalStyle/>
+    </BrowserRouter>
+  </ThemeProvider>
   )
-}
 
+}
